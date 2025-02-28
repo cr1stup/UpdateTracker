02ecd902ca68fbe0806f44d60581012242f15dd2
cr1stup <scaleupgo@gmail.com>
2025-02-28 13:04:27 +0300

    feat(Scrapper): add chat controller

---
+++ scrapper/src/main/java/backend/academy/scrapper/controller/ChatController.java
@@ -0,0 +1,16 @@
+package backend.academy.scrapper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tg-chat/{id}")
+@RequiredArgsConstructor
+public class ChatController {
+
+    @PostMapping
+    public void registerChat(@PathVariable Long id) {}
+}
