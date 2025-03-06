9433dcf4d712604b73327b25c8f80b1ecb37f4f4
cr1stup <scaleupgo@gmail.com>
2025-03-06 07:50:07 +0300

    feat(Bot): update BotMessages

--- bot/src/main/java/backend/academy/bot/util/BotMessages.java
+++ bot/src/main/java/backend/academy/bot/util/BotMessages.java
@@ -1,21 +1,28 @@
 package backend.academy.bot.util;
 
 public interface BotMessages {
-    String UNKNOWN_COMMAND = "unknown command";
+    String UNKNOWN_COMMAND = "неизвестная команда";
 
     String START_NAME = "/start";
-    String START_DESCRIPTION = "start description";
-    String START_HANDLE = "start message";
+    String START_DESCRIPTION = "регистрация пользователя";
+    String START_HANDLE = """
+        Добро пожаловать в бот для отслеживания обновлений по ссылкам!
+        Введите /help для просмортра доступных команд
+        """;
 
     String HELP_NAME = "/help";
-    String HELP_DESCRIPTION = "help description";
+    String HELP_DESCRIPTION = "список команд";
 
     String TRACK_NAME = "/track";
-    String TRACK_DESCRIPTION = "track description";
+    String TRACK_DESCRIPTION = "начать отслеживать ссылку";
 
     String UNTRACK_NAME = "/untrack";
-    String UNTRACK_DESCRIPTION = "untrack description";
+    String UNTRACK_DESCRIPTION = "удалить ссылку из отслеживаемых";
 
     String LIST_NAME = "/list";
-    String LIST_DESCRIPTION = "list description";
+    String LIST_DESCRIPTION = "вывести список отслеживаемых ссылок";
+
+    String EMPTY_LIST = "список отслеживаемых ссылок пуст";
+
+    String UPDATE_MESSAGE = "Обновление по вашей ссылке";
 }
