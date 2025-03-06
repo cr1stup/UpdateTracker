9c63b581c0768c001d2153a45787961731538ba0
cr1stup <scaleupgo@gmail.com>
2025-03-06 08:01:06 +0300

    feat(Scrapper): add Link dto

---
+++ scrapper/src/main/java/backend/academy/scrapper/dto/Link.java
@@ -0,0 +1,33 @@
+package backend.academy.scrapper.dto;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Link {
+    private long id;
+    private String url;
+    private List<String> tags;
+    private List<String> filters;
+    private OffsetDateTime updatedAt;
+    private OffsetDateTime lastCheckedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Link link = (Link) o;
+        return Objects.equals(url, link.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url);
+    }
+}
