eaf9aac142b8dcb5966e3e95738bbc9945a7cf38
cr1stup <scaleupgo@gmail.com>
2025-03-06 08:37:43 +0300

    feat(Scrapper): add client interface

---
+++ scrapper/src/main/java/backend/academy/scrapper/client/link/LinkClient.java
@@ -0,0 +1,21 @@
+package backend.academy.scrapper.client.link;
+
+import backend.academy.scrapper.dto.LinkInformation;
+import org.springframework.web.reactive.function.client.WebClient;
+import java.net.URI;
+
+public interface LinkClient {
+
+    boolean isSupport(URI url);
+
+    LinkInformation fetchInformation(URI url);
+
+    default <T> T executeRequest(WebClient webClient, String uri, Class<T> type, T defaultValue) {
+        return webClient.get()
+            .uri(uri)
+            .retrieve()
+            .bodyToMono(type)
+            .onErrorReturn(defaultValue)
+            .block();
+    }
+}
