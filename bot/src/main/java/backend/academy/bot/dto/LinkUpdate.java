97421debf4bd419373e2d2e941e153c56f001aeb
cr1stup <scaleupgo@gmail.com>
2025-03-06 09:01:10 +0300

    feat(Bot): add link updates controller

---
+++ bot/src/main/java/backend/academy/bot/dto/LinkUpdate.java
@@ -0,0 +1,12 @@
+package backend.academy.bot.dto;
+
+import jakarta.validation.constraints.NotNull;
+import java.net.URI;
+import java.util.List;
+
+public record LinkUpdate(
+    @NotNull Long id,
+    @NotNull URI url,
+    @NotNull String description,
+    @NotNull List<Long> tgChatIds
+) {}
