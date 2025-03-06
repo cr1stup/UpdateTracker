49384424a83c2d9985654f8a2c8d13e02521b493
cr1stup <scaleupgo@gmail.com>
2025-03-06 08:34:42 +0300

    feat(Scrapper): add scheduler to scrapper config

--- scrapper/src/main/java/backend/academy/scrapper/config/ScrapperConfig.java
+++ scrapper/src/main/java/backend/academy/scrapper/config/ScrapperConfig.java
@@ -1,11 +1,19 @@
 package backend.academy.scrapper.config;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
+import java.time.Duration;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ScrapperConfig(@NotEmpty String githubToken, StackOverflowCredentials stackOverflow) {
+public record ScrapperConfig(@NotNull Scheduler scheduler, @NotEmpty String githubToken, StackOverflowCredentials stackOverflow) {
     public record StackOverflowCredentials(@NotEmpty String key, @NotEmpty String accessToken) {}
+
+    public record Scheduler(
+        boolean enable,
+        @NotNull Duration interval,
+        @NotNull Duration forceCheckDelay
+    ) {}
 }
