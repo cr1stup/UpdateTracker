23712d98516c65b7e4e6ef16ae694cbbd290fb98
cr1stup <scaleupgo@gmail.com>
2025-03-06 07:58:12 +0300

    feat(Bot): add FilterStateHandler

---
+++ bot/src/main/java/backend/academy/bot/handler/state/FilterStateHandler.java
@@ -0,0 +1,51 @@
+package backend.academy.bot.handler.state;
+
+import backend.academy.bot.repository.BotRepository;
+import backend.academy.bot.service.BotService;
+import backend.academy.bot.util.ChatLimits;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FilterStateHandler implements StateHandler {
+
+    private final BotService botService;
+    private final BotRepository botRepository;
+    private final static int MAX_FILTERS = ChatLimits.MAX_FILTERS;
+
+    @Override
+    public BotState state() {
+        return BotState.FILTER;
+    }
+
+    @Override
+    public SendMessage handle(Update update) {
+        Long chatId = update.message().chat().id();
+        String[] userText = update.message().text().split(" ");
+
+        if (userText.length > MAX_FILTERS) {
+            return new SendMessage(chatId, "Введите меньше %d фильтров".formatted(MAX_FILTERS));
+        } else if (!"null".equalsIgnoreCase(userText[0])) {
+            botRepository.setFilters(chatId, Arrays.asList(userText));
+        }
+
+        var response = botService.linkUrlToUser(
+            botRepository.getLink(chatId),
+            chatId,
+            botRepository.getTags(chatId),
+            botRepository.getFilters(chatId)
+        );
+
+        botRepository.setState(chatId, BotState.START);
+
+        if (response.isError()) {
+            return new SendMessage(chatId, "Не удалось сохранить ссылку: " + response.getErrorMessage());
+        }
+
+        return new SendMessage(chatId, "Ваша ссылка успешно сохранена для отслеживания!");
+    }
+}
