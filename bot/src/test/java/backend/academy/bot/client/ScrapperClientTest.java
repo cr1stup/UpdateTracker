794fcb8aa18b9820dfc5bf92287739262e6ae191
cr1stup <scaleupgo@gmail.com>
2025-03-27 09:49:57 +0300

    test(Bot): add test success request to ScrapperClientTest
    add test success request to ScrapperClientTest
    and separate mocks into util class

--- bot/src/test/java/backend/academy/bot/client/ScrapperClientTest.java
+++ bot/src/test/java/backend/academy/bot/client/ScrapperClientTest.java
@@ -1,14 +1,12 @@
 package backend.academy.bot.client;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
-
+import backend.academy.bot.client.dto.AddLinkRequest;
+import backend.academy.bot.client.dto.LinkResponse;
+import backend.academy.bot.client.dto.ListLinksResponse;
+import backend.academy.bot.client.dto.RemoveLinkRequest;
 import backend.academy.bot.dto.ApiErrorResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import java.net.URI;
 import java.util.List;
 import lombok.SneakyThrows;
 import org.assertj.core.api.Assertions;
@@ -22,19 +20,11 @@ import reactor.core.publisher.Mono;
 @WireMockTest(httpPort = 9090)
 public class ScrapperClientTest {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     @SneakyThrows
     @Test
     @DisplayName("correct error handling from scrapper client")
     public void testHandlingErrorResponse() {
-        stubFor(get(urlPathMatching("/links"))
-                .withHeader("Tg-Chat-Id", equalTo("100"))
-                .willReturn(aResponse()
-                        .withStatus(404)
-                        .withBody(objectMapper.writeValueAsString(
-                                new ApiErrorResponse("Not found", "404", "Not found", "Not found", List.of())))
-                        .withHeader("Content-Type", "application/json")));
+        WireMockStubUtil.mockListLinksNotFound();
         ScrapperClient scrapperClient = scrapperClient();
 
         ApiErrorResponse response = scrapperClient.listLinks(100L).apiErrorResponse();
@@ -42,15 +32,59 @@ public class ScrapperClientTest {
         Assertions.assertThat(response).extracting(ApiErrorResponse::code).isEqualTo("404");
     }
 
+    @SneakyThrows
+    @Test
+    @DisplayName("success get list links and returns correct response from Scrapper client")
+    public void testListLinksReturnCorrectValue() {
+        WireMockStubUtil.mockListLinksSuccess();
+        ScrapperClient scrapperClient = scrapperClient();
+
+        ListLinksResponse response = scrapperClient.listLinks(100L).answer();
+
+        Assertions.assertThat(response)
+            .extracting(ListLinksResponse::links)
+            .isEqualTo(List.of(new LinkResponse(100L, URI.create("https://test.com"), null, null)));
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("success add link and returns correct response from Scrapper client")
+    public void testAddLinkReturnCorrectValue() {
+        WireMockStubUtil.mockAddLinkSuccess();
+        ScrapperClient scrapperClient = scrapperClient();
+
+        LinkResponse response = scrapperClient.addLink(100L,
+            new AddLinkRequest(URI.create("https://test.com"), null, null)).answer();
+
+        Assertions.assertThat(response)
+            .extracting(LinkResponse::id, LinkResponse::url)
+            .contains(100L, URI.create("https://test.com"));
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("success remove link and returns correct response from Scrapper client")
+    public void testRemoveLinkReturnCorrectValue() {
+        WireMockStubUtil.mockRemoveLinkSuccess();
+        ScrapperClient scrapperClient = scrapperClient();
+
+        LinkResponse response =
+            scrapperClient.removeLink(100L, new RemoveLinkRequest(URI.create("https://test.com"))).answer();
+
+        Assertions.assertThat(response)
+            .extracting(LinkResponse::id, LinkResponse::url)
+            .contains(100L, URI.create("https://test.com"));
+    }
+
     private static ScrapperClient scrapperClient() {
         WebClient webClient = WebClient.builder()
-                .defaultStatusHandler(httpStatusCode -> true, clientResponse -> Mono.empty())
-                .baseUrl("http://localhost:9090")
-                .build();
+            .defaultStatusHandler(httpStatusCode -> true, clientResponse -> Mono.empty())
+            .baseUrl("http://localhost:9090")
+            .build();
 
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(
-                        WebClientAdapter.create(webClient))
-                .build();
+                WebClientAdapter.create(webClient))
+            .build();
 
         return httpServiceProxyFactory.createClient(ScrapperClient.class);
     }
---
+++ bot/src/test/java/backend/academy/bot/client/WireMockStubUtil.java
@@ -0,0 +1,78 @@
+package backend.academy.bot.client;
+
+import backend.academy.bot.client.dto.LinkResponse;
+import backend.academy.bot.client.dto.ListLinksResponse;
+import backend.academy.bot.dto.ApiErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URI;
+import java.util.List;
+import lombok.SneakyThrows;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+
+public class WireMockStubUtil {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @SneakyThrows
+    public static void mockListLinksNotFound() {
+        stubFor(get(urlPathMatching("/links"))
+            .withHeader("Tg-Chat-Id", equalTo("100"))
+            .willReturn(aResponse()
+                .withStatus(404)
+                .withHeader("Content-Type", "application/json")
+                .withBody(objectMapper.writeValueAsString(
+                    new ApiErrorResponse("Not found", "404", "Not found", "Not found", List.of())
+                ))
+            )
+        );
+    }
+
+    @SneakyThrows
+    public static void mockListLinksSuccess() {
+        stubFor(get(urlPathMatching("/links"))
+            .withHeader("Tg-Chat-Id", equalTo("100"))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withBody(objectMapper.writeValueAsString(new ListLinksResponse(
+                    List.of(new LinkResponse(100L, URI.create("https://test.com"), null, null)),
+                    1
+                )))
+            )
+        );
+    }
+
+    @SneakyThrows
+    public static void mockAddLinkSuccess() {
+        stubFor(post(urlPathMatching("/links"))
+            .withHeader("Tg-Chat-Id", equalTo("100"))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withBody(objectMapper.writeValueAsString(new LinkResponse(
+                    100L, URI.create("https://test.com"), null, null
+                )))
+            )
+        );
+    }
+
+    @SneakyThrows
+    public static void mockRemoveLinkSuccess() {
+        stubFor(delete(urlPathMatching("/links"))
+            .withHeader("Tg-Chat-Id", equalTo("100"))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withBody(objectMapper.writeValueAsString(new LinkResponse(
+                    100L, URI.create("https://test.com"), null, null
+                )))
+            )
+        );
+    }
+}
