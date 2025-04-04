8a5ba92cac8a4f094dacac2ac2b4f63e036a4984
cr1stup <scaleupgo@gmail.com>
2025-04-04 09:07:53 +0300

    feat(Scrapper): add jpa link repository

---
+++ scrapper/src/main/java/backend/academy/scrapper/repository/jpa/repository/JpaLinkRepository.java
@@ -0,0 +1,13 @@
+package backend.academy.scrapper.repository.jpa.repository;
+
+import backend.academy.scrapper.repository.jpa.entity.LinkEntity;
+import org.springframework.data.domain.Limit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface JpaLinkRepository extends JpaRepository<LinkEntity, Long> {
+    Optional<LinkEntity> findByUrl(String url);
+    List<LinkEntity> findAllByLastCheckedAtBefore(OffsetDateTime from, Limit limit);
+}
