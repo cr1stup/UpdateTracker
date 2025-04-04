85fd5341968252b0ffbfcb2ca467ae65d16a173e
cr1stup <scaleupgo@gmail.com>
2025-04-04 09:06:17 +0300

    feat(Scrapper): add jpa LinkEntity

---
+++ scrapper/src/main/java/backend/academy/scrapper/repository/jpa/entity/LinkEntity.java
@@ -0,0 +1,60 @@
+package backend.academy.scrapper.repository.jpa.entity;
+
+import backend.academy.scrapper.dto.Link;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "link")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String url;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+
+    @Column(name = "last_checked_at")
+    private OffsetDateTime lastCheckedAt;
+
+    @OneToMany(mappedBy = "link", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ChatLinkEntity> chatLinks = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "links")
+    private Set<ChatEntity> chats = new HashSet<>();
+
+    public Link toDto() {
+        return new Link(
+            id,
+            url,
+            description,
+            updatedAt,
+            lastCheckedAt
+        );
+    }
+}
