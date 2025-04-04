337372277856a89136ec7f8aa781d7b9cd1dd5de
cr1stup <scaleupgo@gmail.com>
2025-04-04 09:05:57 +0300

    feat(Scrapper): add jpa ChatEntity

---
+++ scrapper/src/main/java/backend/academy/scrapper/repository/jpa/entity/ChatEntity.java
@@ -0,0 +1,38 @@
+package backend.academy.scrapper.repository.jpa.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "chat")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatEntity {
+    @Id
+    private Long id;
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ChatLinkEntity> chatLinks = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+        name = "chat_link",
+        joinColumns = @JoinColumn(name = "chat_id"),
+        inverseJoinColumns = @JoinColumn(name = "link_id")
+    )
+    private Set<LinkEntity> links = new HashSet<>();
+}
