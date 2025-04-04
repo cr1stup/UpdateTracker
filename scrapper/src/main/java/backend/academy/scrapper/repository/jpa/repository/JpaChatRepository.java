c0c3fcd5c359b67d7e5842163d8acdbaf15b9348
cr1stup <scaleupgo@gmail.com>
2025-04-04 09:07:44 +0300

    feat(Scrapper): add jpa chat repository

---
+++ scrapper/src/main/java/backend/academy/scrapper/repository/jpa/repository/JpaChatRepository.java
@@ -0,0 +1,7 @@
+package backend.academy.scrapper.repository.jpa.repository;
+
+import backend.academy.scrapper.repository.jpa.entity.ChatEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaChatRepository extends JpaRepository<ChatEntity, Long> {
+}
