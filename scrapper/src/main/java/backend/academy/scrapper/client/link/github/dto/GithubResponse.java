8466669f8ef5a20be8356e43bf824d768301b048
cr1stup <scaleupgo@gmail.com>
2025-04-04 09:20:37 +0300

    feat(Scrapper): add github response dto

---
+++ scrapper/src/main/java/backend/academy/scrapper/client/link/github/dto/GithubResponse.java
@@ -0,0 +1,14 @@
+package backend.academy.scrapper.client.link.github.dto;
+
+import java.time.OffsetDateTime;
+
+public record GithubResponse(
+    String title,
+    User user,
+    OffsetDateTime created_at,
+    String body
+) {
+    public record User(
+        String login
+    ) {}
+}
--- scrapper/src/main/java/backend/academy/scrapper/client/link/github/dto/ProfileInfo.java
+++ scrapper/src/main/java/backend/academy/scrapper/client/link/github/dto/ProfileInfo.java
@@ -1,11 +1,11 @@
-package backend.academy.scrapper.client.link.github;
+package backend.academy.scrapper.client.link.github.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.OffsetDateTime;
 
-record RepositoryInfo(
+public record ProfileInfo(
         @JsonProperty("full_name") String fullName,
         @JsonProperty("pushed_at") OffsetDateTime lastModified,
         String description) {
-    public static final RepositoryInfo EMPTY = new RepositoryInfo(null, null, null);
+    public static final ProfileInfo EMPTY = new ProfileInfo(null, null, null);
 }
