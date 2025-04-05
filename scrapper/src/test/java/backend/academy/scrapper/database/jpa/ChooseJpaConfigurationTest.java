7881c5b481dab3130848739fbafe95d7570d433e
cr1stup <scaleupgo@gmail.com>
2025-04-05 10:24:38 +0300

    test(Scrapper): add right jpa configuration test
    work goes through jpa when configuration is jpa

---
+++ scrapper/src/test/java/backend/academy/scrapper/database/jpa/ChooseJpaConfigurationTest.java
@@ -0,0 +1,36 @@
+package backend.academy.scrapper.database.jpa;
+
+import backend.academy.scrapper.database.IntegrationEnvironment;
+import backend.academy.scrapper.service.ChatService;
+import backend.academy.scrapper.service.LinkService;
+import backend.academy.scrapper.service.jpa.JpaChatService;
+import backend.academy.scrapper.service.jpa.JpaLinkService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@TestPropertySource(properties = "app.database-access-type=jpa")
+class ChooseJpaConfigurationTest extends IntegrationEnvironment {
+
+    @Autowired
+    private ChatService chatService;
+
+    @Autowired
+    private LinkService linkService;
+
+    @Test
+    @DisplayName("chat service is jpa when config jpa")
+    void testChatServiceIsJpa() {
+        assertThat(chatService).isInstanceOf(JpaChatService.class);
+    }
+
+    @Test
+    @DisplayName("link service is jpa when config jpa")
+    void testLinkServiceIsJpa() {
+        assertThat(linkService).isInstanceOf(JpaLinkService.class);
+    }
+}
