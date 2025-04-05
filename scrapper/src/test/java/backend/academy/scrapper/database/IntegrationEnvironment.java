e04858ad969b57ef34df06273197cbc9ecbc3e70
cr1stup <scaleupgo@gmail.com>
2025-04-05 10:19:46 +0300

    test(Scrapper): add postgres testcontainers

---
+++ scrapper/src/test/java/backend/academy/scrapper/database/IntegrationEnvironment.java
@@ -0,0 +1,51 @@
+package backend.academy.scrapper.database;
+
+import java.nio.file.Path;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.DirectoryResourceAccessor;
+import lombok.SneakyThrows;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class IntegrationEnvironment {
+    public static final PostgreSQLContainer<?> POSTGRES;
+
+    static {
+        POSTGRES = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("scrapper")
+            .withUsername("postgres")
+            .withPassword("postgres");
+
+        if (!POSTGRES.isRunning()) {
+            POSTGRES.start();
+            runMigrations(POSTGRES);
+        }
+    }
+
+    @SneakyThrows
+    private static void runMigrations(JdbcDatabaseContainer<?> container) {
+        Database database = DatabaseFactory.getInstance()
+            .findCorrectDatabaseImplementation(new JdbcConnection(container.createConnection("")));
+
+        Liquibase liquibase = new Liquibase(
+            "master.xml",
+            new DirectoryResourceAccessor(Path.of("E:\\InteljCode\\java-cr1stup\\migrations")),
+            database
+        );
+        liquibase.update();
+    }
+
+    @DynamicPropertySource
+    static void jdbcProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+}
