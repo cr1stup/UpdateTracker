7ee2a2481c2623df178833b636a95cc877f035d9
cr1stup <scaleupgo@gmail.com>
2025-04-17 19:23:54 +0300

    feat(Bot): add kafka updates listener

---
+++ bot/src/main/java/backend/academy/bot/kafka/KafkaUpdatesListener.java
@@ -0,0 +1,22 @@
+package backend.academy.bot.kafka;
+
+import backend.academy.bot.dto.LinkUpdate;
+import backend.academy.bot.service.LinkNotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaUpdatesListener {
+
+    private final LinkNotificationService linkNotificationService;
+
+    @KafkaListener(topics = "${app.kafka.updates-topic-name}", groupId = "bot")
+    @RetryableTopic(attempts = "1", dltStrategy = DltStrategy.FAIL_ON_ERROR, dltTopicSuffix = "_dlq")
+    public void listenUpdate(LinkUpdate update) {
+        linkNotificationService.notifyLinkUpdate(update);
+    }
+}
