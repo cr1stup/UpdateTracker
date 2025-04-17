a57a6743a7be572dac1fd61cf9720d39476d9980
cr1stup <scaleupgo@gmail.com>
2025-04-17 19:36:30 +0300

    feat(Scrapper): add redis configuration

--- scrapper/src/main/java/backend/academy/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/backend/academy/scrapper/ScrapperApplication.java
@@ -4,8 +4,10 @@ import backend.academy.scrapper.config.ScrapperConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 @EnableConfigurationProperties({ScrapperConfig.class})
 public class ScrapperApplication {
     public static void main(String[] args) {
---
+++ scrapper/src/main/java/backend/academy/scrapper/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package backend.academy.scrapper.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(cacheConfig)
+            .build();
+    }
+}
