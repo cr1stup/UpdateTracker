a0b49f026c2c71d3fa4c28c10efa495881f701bc
cr1stup <scaleupgo@gmail.com>
2025-04-21 17:17:55 +0300

    feat(Scrapper): update configs
    update database and redis configurations

--- scrapper/src/main/java/backend/academy/scrapper/config/DatabaseConfig.java
+++ scrapper/src/main/java/backend/academy/scrapper/config/DatabaseConfig.java
@@ -2,19 +2,25 @@ package backend.academy.scrapper.config;
 
 import backend.academy.scrapper.client.link.ClientFactory;
 import backend.academy.scrapper.repository.jdbc.JdbcChatLinkRepository;
+import backend.academy.scrapper.repository.jdbc.JdbcChatModeRepository;
 import backend.academy.scrapper.repository.jdbc.JdbcChatRepository;
 import backend.academy.scrapper.repository.jdbc.JdbcFilterRepository;
 import backend.academy.scrapper.repository.jdbc.JdbcLinkRepository;
 import backend.academy.scrapper.repository.jdbc.JdbcTagRepository;
 import backend.academy.scrapper.repository.jpa.repository.JpaChatLinkRepository;
+import backend.academy.scrapper.repository.jpa.repository.JpaChatModeRepository;
 import backend.academy.scrapper.repository.jpa.repository.JpaChatRepository;
 import backend.academy.scrapper.repository.jpa.repository.JpaFilterRepository;
 import backend.academy.scrapper.repository.jpa.repository.JpaLinkRepository;
+import backend.academy.scrapper.repository.jpa.repository.JpaModeRepository;
 import backend.academy.scrapper.repository.jpa.repository.JpaTagRepository;
+import backend.academy.scrapper.service.BatchUpdateService;
 import backend.academy.scrapper.service.ChatService;
 import backend.academy.scrapper.service.LinkService;
+import backend.academy.scrapper.service.jdbc.JdbcChatModeService;
 import backend.academy.scrapper.service.jdbc.JdbcChatService;
 import backend.academy.scrapper.service.jdbc.JdbcLinkService;
+import backend.academy.scrapper.service.jpa.JpaChatModeService;
 import backend.academy.scrapper.service.jpa.JpaChatService;
 import backend.academy.scrapper.service.jpa.JpaLinkService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -26,8 +32,8 @@ public class DatabaseConfig {
 
     @Bean
     @ConditionalOnProperty(name = "app.database-access-type", havingValue = "jdbc")
-    public ChatService jdbcChatService(JdbcChatRepository chatRepository) {
-        return new JdbcChatService(chatRepository);
+    public ChatService jdbcChatService(JdbcChatRepository chatRepository, JdbcChatModeService jdbcChatModeService) {
+        return new JdbcChatService(chatRepository, jdbcChatModeService);
     }
 
     @Bean
@@ -45,8 +51,8 @@ public class DatabaseConfig {
 
     @Bean
     @ConditionalOnProperty(name = "app.database-access-type", havingValue = "jpa")
-    public ChatService jpaChatService(JpaChatRepository jpaChatRepository) {
-        return new JpaChatService(jpaChatRepository);
+    public ChatService jpaChatService(JpaChatRepository jpaChatRepository, JpaChatModeService jpaChatModeService) {
+        return new JpaChatService(jpaChatRepository, jpaChatModeService);
     }
 
     @Bean
@@ -60,5 +66,17 @@ public class DatabaseConfig {
             JpaFilterRepository filterRepository) {
         return new JpaLinkService(
                 clientFactory, chatLinkRepository, chatRepository, linkRepository, tagRepository, filterRepository);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "app.database-access-type", havingValue = "jpa")
+    public JpaChatModeService jpaChatModeService(JpaChatModeRepository chatModeRepository, JpaModeRepository modeRepository, BatchUpdateService batchUpdateService) {
+        return new JpaChatModeService(chatModeRepository, modeRepository, batchUpdateService);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "app.database-access-type", havingValue = "jdbc")
+    public JdbcChatModeService jdbcChatModeService(JdbcChatModeRepository chatModeRepository, BatchUpdateService batchUpdateService) {
+        return new JdbcChatModeService(chatModeRepository, batchUpdateService);
     }
 }
--- scrapper/src/main/java/backend/academy/scrapper/config/RedisConfig.java
+++ scrapper/src/main/java/backend/academy/scrapper/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package backend.academy.scrapper.config;
 
+import backend.academy.scrapper.dto.LinkUpdate;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -7,8 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
@@ -24,5 +28,14 @@ public class RedisConfig {
         return RedisCacheManager.builder(redisConnectionFactory)
             .cacheDefaults(cacheConfig)
             .build();
+    }
+
+    @Bean
+    public RedisTemplate<String, LinkUpdate> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, LinkUpdate> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(LinkUpdate.class));
+        return template;
     }
 }
