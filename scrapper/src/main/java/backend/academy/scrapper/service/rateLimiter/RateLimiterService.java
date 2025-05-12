e756653d43561bca0e4eebe42ec75274a528d43d
cr1stup <scaleupgo@gmail.com>
2025-05-12 12:12:12 +0300

    feat(Scrapper): add rate limiter

---
+++ scrapper/src/main/java/backend/academy/scrapper/service/rateLimiter/RateLimiterService.java
@@ -0,0 +1,60 @@
+package backend.academy.scrapper.service.rateLimiter;
+
+import backend.academy.scrapper.config.RateLimiterProperties;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableConfigurationProperties(RateLimiterProperties.class)
+@RequiredArgsConstructor
+@Slf4j
+public class RateLimiterService implements Filter {
+
+    private final RateLimiterProperties properties;
+    private final Map<String, Bucket> ipBuckets = new ConcurrentHashMap<>();
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String ip = getClientIp(httpRequest);
+
+        Bucket bucket = ipBuckets.computeIfAbsent(ip, this::newBucket);
+
+        if (bucket.tryConsume(1)) {
+            chain.doFilter(request, response);
+        } else {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setStatus(429);
+            httpResponse.getWriter().write("Too many requests from IP: " + ip);
+            log.warn("Too many requests from IP: {}", ip);
+        }
+    }
+
+    private Bucket newBucket(String ip) {
+        Refill refill = Refill.greedy(properties.refillTokens(), properties.refillDuration());
+        Bandwidth limit = Bandwidth.classic(properties.capacity(), refill);
+        return Bucket.builder().addLimit(limit).build();
+    }
+
+    private String getClientIp(HttpServletRequest request) {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        return xfHeader != null ? xfHeader.split(",")[0] : request.getRemoteAddr();
+    }
+}
