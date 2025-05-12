2194c3916141d7041f5df2a6d492ae743d6e075f
cr1stup <scaleupgo@gmail.com>
2025-05-12 13:23:43 +0300

    test(Scrapper): add botClient retry test

---
+++ scrapper/src/test/java/backend/academy/scrapper/client/BotClientRetryTest.java
@@ -0,0 +1,54 @@
+package backend.academy.scrapper.client;
+
+import backend.academy.scrapper.dto.LinkUpdate;
+import backend.academy.scrapper.service.update.HttpUpdateService;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import java.util.List;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+@WireMockTest(httpPort = 9090)
+@TestPropertySource(properties = {
+    "client.bot-url=http://localhost:9090",
+    "retry.max-attempts=2",
+    "retry.retryable-codes=503"
+})
+@SpringBootTest
+public class BotClientRetryTest {
+
+    @Autowired
+    private HttpUpdateService updateService;
+
+    private final LinkUpdate update = new LinkUpdate(1L, null, "description", List.of(1L), false);
+
+    @SneakyThrows
+    @Test
+    @DisplayName("botClient retry should work for error 503")
+    public void testRetryForError503() {
+        WireMockStubUtil.mockBotError503();
+
+        try {
+            updateService.sendUpdatesToUsers(update);
+        } catch (Exception e) {
+            WireMock.verify(3, postRequestedFor(urlEqualTo("/updates")));
+        }
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("botClient retry should not work for error 404")
+    public void testRetryForError404() {
+        WireMockStubUtil.mockBotError404();
+
+        updateService.sendUpdatesToUsers(update);
+
+        WireMock.verify(1, postRequestedFor(urlEqualTo("/updates")));
+    }
+}
---
+++ scrapper/src/test/java/backend/academy/scrapper/client/WireMockStubUtil.java
@@ -0,0 +1,43 @@
+package backend.academy.scrapper.client;
+
+import backend.academy.scrapper.dto.ApiErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import lombok.SneakyThrows;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+
+public class WireMockStubUtil {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @SneakyThrows
+    public static void mockBotError503() {
+        stubFor(
+            post(urlPathMatching("/updates"))
+                .willReturn(aResponse()
+                    .withStatus(503)
+                    .withBody(objectMapper.writeValueAsString(
+                        new ApiErrorResponse("Not found", "503", "Not found", "Not found", List.of())
+                    ))
+                    .withHeader("Content-Type", "application/json")
+                )
+        );
+    }
+
+    @SneakyThrows
+    public static void mockBotError404() {
+        stubFor(
+            post(urlPathMatching("/updates"))
+                .willReturn(aResponse()
+                    .withStatus(404)
+                    .withBody(objectMapper.writeValueAsString(
+                        new ApiErrorResponse("Not found", "404", "Not found", "Not found", List.of())
+                    ))
+                    .withHeader("Content-Type", "application/json")
+                )
+        );
+    }
+}
